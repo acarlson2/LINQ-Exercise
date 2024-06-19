3fd4e8f9b4c049bc2d93960234624eb2a46f3038
Aaron Carlson <acarlson2@carthage.edu>
2024-06-19 13:19:43 -0500

    exercise complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>();
+
+            games.Add("Pokemon Black 2");
+            games.Add("Warframe");
+            games.Add("The Legend of Zelda: Tears of the Kingdom");
+            games.Add("Super Smash Brothers Ultimate");
+            games.Add("Horizon: Zero Dawn");
+            games.Add("Splatoon 3");
+            games.Add("Spider-Man 2");
+
+            List<string> sorted = games.OrderBy(g => g.Length).ToList();
+
+            for(int i = 0; i < sorted.Count; i++)
+            {
+                Console.WriteLine(sorted[i]);
+            }
         }
     }
 }
